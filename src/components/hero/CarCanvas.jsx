0e2a055b0ad591fb112dc2ee6a340b2c9f33d953
marco <marco.crucitti96@gmail.com>
2025-08-18 23:23:14 +0200

    modifica canvas

--- src/components/hero/CarCanvas.jsx
+++ src/components/hero/CarCanvas.jsx
@@ -1,70 +1,3 @@
-// import { Canvas } from "@react-three/fiber";
-// import {
-//   OrbitControls,
-//   Environment,
-//   ContactShadows,
-//   Html,
-//   useGLTF,
-// } from "@react-three/drei";
-// import { Suspense, useEffect } from "react";
-// import AnimatedModel from "../AnimatedModel";
-// import Loader3D from "../Loader3D";
-
-// export default function CarCanvas({ car, cameraPosition, cars, active }) {
-//   useEffect(() => {
-//     if (!car) return;
-//     useGLTF.preload(car.model);
-//     const next = cars[active + 1];
-//     const prev = cars[active - 1];
-//     if (next) useGLTF.preload(next.model);
-//     if (prev) useGLTF.preload(prev.model);
-//   }, [car, cars, active]);
-
-//   return (
-//     <div className="model-view">
-//       <Canvas
-//         shadows
-//         dpr={[1, 1.75]}
-//         gl={{ powerPreference: "high-performance", antialias: true }}
-//         camera={{ position: cameraPosition, fov: 45 }}
-//       >
-//         <ambientLight intensity={0.8} />
-//         <directionalLight position={[5, 5, 5]} />
-
-//         <Suspense
-//           fallback={
-//             <Html center>
-//               <Loader3D />
-//             </Html>
-//           }
-//         >
-//           <AnimatedModel
-//             key={car.model}
-//             modelPath={car.model}
-//             scaleFactor={car.scale}
-//             rotation={car.rotation}
-//             offset={car.offset}
-//             shadows={false}
-//             inDuration={0.8}
-//           />
-//         </Suspense>
-
-//         <ContactShadows
-//           position={[0, -0.01, 0]}
-//           opacity={0.5}
-//           blur={1.2}
-//           scale={10}
-//           far={5}
-//           resolution={512}
-//         />
-
-//         <OrbitControls makeDefault enableZoom={false} enablePan={false} />
-//         <Environment preset="sunset" />
-//       </Canvas>
-//     </div>
-//   );
-// }
-
 import { Canvas } from "@react-three/fiber";
 import {
   OrbitControls,
@@ -144,7 +77,7 @@ export default function CarCanvas({ car, cameraPosition, cars, active }) {
           resolution={512}
         />
 
-        <OrbitControls makeDefault enableZoom={false} enablePan={false} />
+        <OrbitControls makeDefault enableZoom={true} enablePan={false} />
         <Environment preset="sunset" />
       </Canvas>
     </div>
